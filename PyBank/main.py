aa037e27e9cd08ec00f8ae31a9ff67f5409473bc
Jacob Schreiber <jacobschreiber1997@gmail.com>
2021-04-10 13:01:17 -0500

    some note

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,10 +4,31 @@ import csv
 csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 
 
-def print_data(budget_data):
+date = []
+profitlosses = []
+
+avgprofitlosses = 0
+with open(csvpath) as csvfile:
+    csvReader = csv.reader(csvfile, delimiter=',')
+    next(csvfile)
+    for row in csvReader:
+        date.append(row[0])
+        profitlosses.append(int(row[1]))
+        
+totalmonths = len(date)
+totalprofit = sum(profitlosses)
+
+changetotal = 0
+profit = (profitlosses[1])-(profitlosses[0])
+loss = (profitlosses[1])-(profitlosses[0])
+
+for i in range(0,totalmonths-1):
+    change = (profitlosses[i+1])-(profitlosses[i])
+    if change >= profit:
+        profit = change
+        dateforprofit = dates[i+1]
+    elif
     
-    date = str(budget_data[0])
-    Profitlosses = int(budget_data[1])
     
         
 
