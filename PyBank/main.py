de4c8a9950d0e924a6fc2dd560980caab4a0ebd6
Jacob Schreiber <jacobschreiber1997@gmail.com>
2021-04-10 11:00:55 -0500

    some note

---
+++ PyBank/main.py
@@ -0,0 +1,18 @@
+import os
+import csv
+
+csvpath = "Resources/buget_data.csv"
+
+
+with open(csvpath) as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    print(csvreader)
+    
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    
+    for row in csvreader:
+        print(row)
+        
