8ceb66d58d791f87e77f4120da0bfb7785e50be6
Jacob Schreiber <jacobschreiber1997@gmail.com>
2021-04-10 20:16:51 -0500

    some note

---
+++ PyPoll/main.py
@@ -0,0 +1,17 @@
+import os
+import csv
+
+csvpath = os.path.join('.', 'Resources', 'election_data.csv')
+
+voterid = []
+county = []
+candidate = []
+
+with open(csvpath) as csvfile:
+    csvReader = csv.reader(csvfile, delimiter=',')
+    next(csvfile)
+    for row in csvReader:
+        voterid.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+        
